91e1abdc13946b8fe10e394382581a2f19a4915e
Petra Jaros <peeja@peeja.com>
2023-03-22 11:14:28 -0400

    Get Todo data into m-ld

--- src/components/MeldDebug.tsx
+++ src/components/MeldDebug.tsx
@@ -5,7 +5,11 @@ import N3 from "n3";
 import { useMeld } from "../hooks/useMeld";
 
 // TODO: Stop momentary double-display.
-export const MeldDebug = () => {
+export const MeldDebug = ({
+  prefixes,
+}: {
+  prefixes?: N3.WriterOptions["prefixes"];
+}) => {
   const meld = useMeld();
   const [stream, setStream] = useState<Stream<Quad>>();
   const [turtle, setTurtle] = useState("");
@@ -14,17 +18,18 @@ export const MeldDebug = () => {
     setTurtle("");
 
     if (stream) {
-      const streamWriter = new N3.StreamWriter();
+      const streamWriter = new N3.StreamWriter({ prefixes });
       streamWriter.on("data", (data: string) => {
         setTurtle((prevTurtle) => prevTurtle + data);
       });
+
       streamWriter.import(stream);
 
       return () => {
         streamWriter.destroy();
       };
     }
-  }, [stream]);
+  }, [prefixes, stream]);
 
   useEffect(() => {
     if (meld) {
