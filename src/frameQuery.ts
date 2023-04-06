c90d7977de3258359f72fe087c64d562db40b0d9
Petra Jaros <peeja@peeja.com>
2023-04-06 10:42:52 -0400

    Extract queryForFrame()

--- src/frameQuery.ts
+++ src/frameQuery.ts
@@ -5,8 +5,13 @@ import { Algebra, Factory as SparqlFactory } from "sparqlalgebrajs";
 import dataset from "@graphy/memory.dataset.fast";
 import { DataFactory } from "rdf-data-factory";
 
+// TODO: Is it bad to have these at the module level?
+const df = new DataFactory();
 const engine = new QueryEngine();
 
+/**
+ * Read all quads from an RDF Stream and return them as a promise of an array.
+ */
 const readAll = <Q extends BaseQuad = Quad>(stream: Stream<Q>) => {
   return new Promise<Q[]>((resolve) => {
     const quads: Q[] = [];
@@ -20,34 +25,12 @@ const readAll = <Q extends BaseQuad = Quad>(stream: Stream<Q>) => {
   });
 };
 
-const query = async (input: JsonLdDocument, frameDocument: NodeObject) => {
-  const df = new DataFactory();
+const queryForFrame = async (frameDocument: NodeObject) => {
   const sparql = new SparqlFactory();
-
-  // This whole madness is just to cope with the fact that jsonld.toRDF doesn't
-  // return real Quads. Namely, they're missing the `.equals()` methods.
-  const fixQuad = (q: Quad) => {
-    // @ts-ignore
-    const fixTerm: typeof df.fromTerm = (term) => {
-      return term.termType === "Literal"
-        ? df.literal(term.value, term.datatype)
-        : df.fromTerm(term);
-    };
-
-    const quad = df.quad(
-      fixTerm(q.subject),
-      fixTerm(q.predicate),
-      fixTerm(q.object),
-      fixTerm(q.graph)
-    );
-    return quad;
-  };
 
   const frameRdf = (
     await toRDF(frameDocument, { produceGeneralizedRdf: true })
   ).map(fixQuad);
-  const inputRdf = (await toRDF(input)).map(fixQuad);
-  const inputDataset = dataset().addAll(inputRdf);
 
   const patterns = frameRdf.map((frameQuad) => {
     return sparql.createPattern(
@@ -65,6 +48,33 @@ const query = async (input: JsonLdDocument, frameDocument: NodeObject) => {
     sparql.createBgp(patterns),
     patterns
   );
+  return algebraQuery;
+};
+
+// This whole madness is just to cope with the fact that jsonld.toRDF doesn't
+// return real Quads. Namely, they're missing the `.equals()` methods.
+const fixQuad = (q: Quad) => {
+  // @ts-ignore
+  const fixTerm: typeof df.fromTerm = (term) => {
+    return term.termType === "Literal"
+      ? df.literal(term.value, term.datatype)
+      : df.fromTerm(term);
+  };
+
+  const quad = df.quad(
+    fixTerm(q.subject),
+    fixTerm(q.predicate),
+    fixTerm(q.object),
+    fixTerm(q.graph)
+  );
+  return quad;
+};
+
+const query = async (input: JsonLdDocument, frameDocument: NodeObject) => {
+  const algebraQuery: Algebra.Construct = await queryForFrame(frameDocument);
+
+  const inputRdf = (await toRDF(input)).map(fixQuad);
+  const inputDataset = dataset().addAll(inputRdf);
 
   const quadsStream = await engine.queryQuads(algebraQuery, {
     sources: [inputDataset],
