012d4312bb0ea084ebf96c65247f2c55e3330e74
Petra Jaros <peeja@peeja.com>
2023-05-07 16:06:54 -0400

    Refactor JsonLDDocument a bit

--- src/JsonLDDocument.ts
+++ src/JsonLDDocument.ts
@@ -1,5 +1,8 @@
 type Iri<Prefix extends string, Suffix extends string> = `${Prefix}:${Suffix}`;
 
+export type NextContext<OuterContext, InnerContext> = OuterContext &
+  InnerContext;
+
 type _NodeObject<PropertyTypes, ActiveContext, Self> = {
   [K in keyof ActiveContext]?: ActiveContext[K] extends keyof PropertyTypes
     ? PropertyTypes[ActiveContext[K]]
@@ -10,14 +13,19 @@ type _NodeObject<PropertyTypes, ActiveContext, Self> = {
   [K in keyof PropertyTypes]?: PropertyTypes[K];
 };
 
-type NextContext<OuterContext, InnerContext> = OuterContext & InnerContext;
-
-type NodeObject<PropertyTypes, OuterContext, Self> = Self extends {
-  "@context": infer LocalContext;
-}
-  ? Self &
-      _NodeObject<PropertyTypes, NextContext<OuterContext, LocalContext>, Self>
-  : Self & _NodeObject<PropertyTypes, NextContext<OuterContext, {}>, Self>;
+export type NodeObject<PropertyTypes, OuterContext, Self> = Self &
+  _NodeObject<
+    PropertyTypes,
+    NextContext<
+      OuterContext,
+      Self extends {
+        "@context": infer LocalContext;
+      }
+        ? LocalContext
+        : {}
+    >,
+    Self
+  >;
 
 export type JsonLDDocument<PropertyTypes, OuterContext, Self> = NodeObject<
   PropertyTypes,
