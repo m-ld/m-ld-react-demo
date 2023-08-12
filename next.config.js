7ffcc6a7d0fa67cd38623d52f190226d2d943c62
Petra Jaros <peeja@peeja.com>
2023-08-12 10:51:10 -0400

    Use xQL!

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => ({
+    ...config,
+    experiments: {
+      // Should only be required while we're using jsonld.js for contexts
+      // (because of how it uses http-client, which uses ky). Should go away
+      // when we switch to jsonld-context-parser.
+      topLevelAwait: true,
+    },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/MeldDebug.tsx
+++ src/components/MeldDebug.tsx
@@ -3,13 +3,108 @@ import { Quad, Stream } from "@rdfjs/types";
 import { MeldReadState } from "@m-ld/m-ld";
 import N3 from "n3";
 import { useMeld } from "../hooks/useMeld";
+import { prettyPrintJson } from "pretty-print-json";
 
-// TODO: Stop momentary double-display.
+import { query as runQuery } from "xql";
+
+import type { JsonValue } from "type-fest";
+import Head from "next/head";
+
 export const MeldDebug = ({
   prefixes,
 }: {
   prefixes?: N3.WriterOptions["prefixes"];
-}) => {
+}) => (
+  <>
+    <Query />
+    <Turtle prefixes={prefixes} />
+  </>
+);
+
+const INITIAL_QUERY = `[
+  {
+    "@context": {
+      "icaltzd": "http://www.w3.org/2002/12/cal/icaltzd#"
+    },
+    "icaltzd:summary": "?",
+    "icaltzd:status": "?"
+  }
+]
+`;
+
+const parseQuery = (queryString: string): JsonValue | undefined => {
+  try {
+    return JSON.parse(queryString);
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      return undefined;
+    } else {
+      throw e;
+    }
+  }
+};
+
+const Query = () => {
+  const [queryString, setQueryString] = useState(INITIAL_QUERY);
+  const [query, setQuery] = useState<JsonValue>({});
+  const [valid, setValid] = useState(true);
+
+  useEffect(() => {
+    const nextQuery = parseQuery(queryString);
+    if (nextQuery) {
+      setQuery(nextQuery);
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  }, [queryString]);
+
+  return (
+    <>
+      <textarea
+        value={queryString}
+        onChange={(e) => setQueryString(e.target.value)}
+        style={{
+          width: "100%",
+          height: "200px",
+          boxShadow: "none",
+          outline: valid ? "none" : "1px solid red",
+        }}
+      />
+      <QueryResults query={query} />
+    </>
+  );
+};
+
+const QueryResults = ({ query }: { query: JsonValue }) => {
+  const meld = useMeld();
+  const [queryResults, setQueryResults] = useState<JsonValue>();
+
+  useEffect(() => {
+    if (meld) {
+      runQuery(meld, query).then(setQueryResults);
+    }
+  }, [meld, query]);
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/pretty-print-json@2.0/dist/css/pretty-print-json.css"
+        />
+      </Head>
+      <pre
+        dangerouslySetInnerHTML={{
+          __html: prettyPrintJson.toHtml(queryResults),
+        }}
+      />
+    </>
+  );
+};
+
+// TODO: Stop momentary double-display.
+const Turtle = ({ prefixes }: { prefixes?: N3.WriterOptions["prefixes"] }) => {
   const meld = useMeld();
   const [stream, setStream] = useState<Stream<Quad>>();
   const [turtle, setTurtle] = useState("");
